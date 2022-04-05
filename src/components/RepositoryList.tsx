ab84b564f56f84d7ec2d60e1ab7a424ed261093d
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-04-05 10:36:51 -0300

    Adicionado o typescript aos componentes

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useState, useEffect } from "react";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("http://api.github.com/orgs/rocketseat/repos")
